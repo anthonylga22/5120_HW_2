1c81e10eaecab395ddd63ae7ea58ac381fedfc25
anthonylga22 <anthonymissa@gmail.com>
2023-04-18 23:27:05 -0700

    First file almost working

--- pulsar_classification.py
+++ pulsar_classification.py
@@ -6,53 +6,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+import pickle
 
 # In this section, you can use a search engine to look for the functions that will help you implement the following steps
 
 # Load dataset and show basic statistics
-# 1. Show dataset size (dimensions)
-# 2. Show what column names exist for the 9 attributes in the dataset
-# 3. Show the distribution of target_class column
-# 4. Show the percentage distribution of target_class column
-
-# Load Dataset
 dataset = pd.read_csv('pulsar_stars.csv')
-
+# 1. Show dataset size (dimensions)
+print("Data Size: ", dataset.shape)
+# 2. Show what column names exist for the 9 attributes in the dataset
+print("Column Names: ", list(dataset.columns))
+# 3. Show the distribution of target_class column
+print("Distribution of the target class: ", dataset['target_class'].value_counts())
+# 4. Show the percentage distribution of target_class column
+print("Percentage Distribution of class: ", dataset['target_class'].value_counts(normalize=True) * 100)
 
 
 # Separate predictor variables from the target variable (X and y as we did in the class)
-X = dataset.iloc[:,:-1] # Gets all the rows (except for the last one because it's the output)
-y = dataset.iloc[:,-1] # Gets all the columns
-
-
-
-
+X = dataset.iloc[:, :-1] # Gets all the rows (except for the last one because it's the output)
+y = dataset.iloc[:, -1] # Gets all the columns
 
 # Create train and test splits for model development. Use the 80% and 20% split ratio
-# Name them as X_train, X_test, y_train, and y_test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=20, random_state=0) 
-
-#X_train = [] # Remove this line after implementing train test split
-#X_test = [] # Remove this line after implementing train test split
-
-
-
-
-
-# Standardize the features (Import StandardScaler here)
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) 
 
 # Below is the code to convert X_train and X_test into data frames for the next steps
 cols = X_train.columns
 X_train = pd.DataFrame(X_train, columns=[cols])
 X_test = pd.DataFrame(X_test, columns=[cols])
 
+np.isnan(np.mat.any())
 
-
-
+# Standardize the features (Import StandardScaler here)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
 
 
 # Train SVM with the following parameters.
@@ -62,22 +50,24 @@ X_test = pd.DataFrame(X_test, columns=[cols])
 classifier = SVC(kernel='rbf', C=10.0, gamma=0.3)
 classifier.fit(X_train, y_train)
 
-
-
-
 # Test the above developed SVC on unseen pulsar dataset samples
 y_pred = classifier.predict(X_test)
 
+cm = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix: ")
+print(cm)
+
 
 # compute and print accuracy score
-print(classifier.predict(sc.transform([[30, 87000]])))
+print("The Accuracy Score: ", accuracy_score(y_test, y_pred))
 
 
 
 
 
 # Save your SVC model (whatever name you have given your model) as .sav to upload with your submission
-# You can use the library pickle to save and load your model for this assignment
+# You can use the library pickle to save and load your model for this assignme4nt
 
 
 
