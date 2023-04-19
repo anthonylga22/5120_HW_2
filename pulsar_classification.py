f22d5fac71ea25fc61e0809c88661c5f55284837
Anthony Lizarraga <anthonymissa@gmail.com>
2023-04-19 10:07:27 -0700

    pulsar_classificaton finished

--- pulsar_classification.py
+++ pulsar_classification.py
@@ -6,98 +6,75 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import recall_score, precision_score
+
 import pickle
 
-# In this section, you can use a search engine to look for the functions that will help you implement the following steps
-
-# Load dataset and show basic statistics
+# Load dataset
 dataset = pd.read_csv('pulsar_stars.csv')
-# 1. Show dataset size (dimensions)
-print("Data Size: ", dataset.shape)
-# 2. Show what column names exist for the 9 attributes in the dataset
-print("Column Names: ", list(dataset.columns))
-# 3. Show the distribution of target_class column
-print("Distribution of the target class: ", dataset['target_class'].value_counts())
-# 4. Show the percentage distribution of target_class column
-print("Percentage Distribution of class: ", dataset['target_class'].value_counts(normalize=True) * 100)
 
+print("Data Size: ", dataset.shape) # Dataset size (dimensions)
+print("Column Names: ", list(dataset.columns)) # Column names that exist for the attributes in the dataset
+print("Distribution of the target class: ", dataset['target_class'].value_counts()) # 3. Distribution of target_class column
+print("Percentage Distribution of class: ", dataset['target_class'].value_counts(normalize=True) * 100) # 4. Percentage distribution of target_class column
 
-# Separate predictor variables from the target variable (X and y as we did in the class)
-X = dataset.iloc[:, :-1] # Gets all the rows (except for the last one because it's the output)
-y = dataset.iloc[:, -1] # Gets all the columns
+# Separate predictor variables from the target variable
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
 
-# Create train and test splits for model development. Use the 80% and 20% split ratio
+# Create train and test splits for model development using 80/20 split ratio
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) 
 
-# Below is the code to convert X_train and X_test into data frames for the next steps
-cols = X_train.columns
-X_train = pd.DataFrame(X_train, columns=[cols])
-X_test = pd.DataFrame(X_test, columns=[cols])
-
-np.isnan(np.mat.any())
-
-# Standardize the features (Import StandardScaler here)
+# Standardize the features
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
-
-# Train SVM with the following parameters.
-   #    1. RBF kernel
-   #    2. C=10.0 (Higher value of C means fewer outliers)
-   #    3. gamma = 0.3
+# Train SVM
 classifier = SVC(kernel='rbf', C=10.0, gamma=0.3)
 classifier.fit(X_train, y_train)
 
-# Test the above developed SVC on unseen pulsar dataset samples
+# Test developed SVC on unseen pulsar dataset samples
 y_pred = classifier.predict(X_test)
 
-cm = confusion_matrix(y_test, y_pred)
+# Accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print("The Accuracy Score: ", accuracy)
 
+# Save SVC model
+filename = 'pulsarClassifier.sav'
+pickle.dump(classifier, open(filename, "wb"))
+
+
+
+
+# Confusion matrix
+cm = confusion_matrix(y_test, y_pred)
 print("Confusion Matrix: ")
 print(cm)
 
-
-# compute and print accuracy score
-print("The Accuracy Score: ", accuracy_score(y_test, y_pred))
-
-
-
-
-
-# Save your SVC model (whatever name you have given your model) as .sav to upload with your submission
-# You can use the library pickle to save and load your model for this assignme4nt
-
-
-
-
-
-
-
-
-
-# Optional: You can print test results of your model here if you want. Otherwise implement them in evaluation.py file
-# Get and print confusion matrix
-cm = [[]]
-# Below are the metrics for computing classification accuracy, precision, recall and specificity
+# Metrics for computing classification accuracy, precision, recall and specificity
 TP = cm[0,0]
 TN = cm[1,1]
 FP = cm[0,1]
 FN = cm[1,0]
 
 
-
 # Compute Precision and use the following line to print it
-precision = 0 # Change this line to implement Precision formula
+precision = precision_score(y_test, y_pred)
+print('Precision SKLEARN : {0:0.3f}'.format(precision))
+
+precision = TP / (TP+FP)
 print('Precision : {0:0.3f}'.format(precision))
 
-
 # Compute Recall and use the following line to print it
-recall = 0 # Change this line to implement Recall formula
+recall = recall_score(y_test, y_pred)
+print('Recall or Sensitivity SKLEARN: {0:0.3f}'.format(recall))
+
+recall = TP / (TP+FN)
 print('Recall or Sensitivity : {0:0.3f}'.format(recall))
 
 # Compute Specificity and use the following line to print it
-specificity = 0 # Change this line to implement Specificity formula
+specificity = TN / (TN+FP)
 print('Specificity : {0:0.3f}'.format(specificity))
 
